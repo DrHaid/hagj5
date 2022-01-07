e2ab21623e5a5055a0b773b7e047f40a3eda9f30
DrHaid <40427630+DrHaid@users.noreply.github.com>
2022-01-07 23:41:45 +0100

    main menu scene

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuController : MonoBehaviour
+{
+  public GameObject Info;
+  public GameObject PressStart;
+  public GameObject MusicPlayer;
+  private bool infoShown;
+
+  private void Start()
+  {
+    DontDestroyOnLoad(MusicPlayer);
+  }
+
+  public void ShowInfo()
+  {
+    Info.SetActive(true);
+    PressStart.SetActive(false);
+    infoShown = true;
+  }
+
+  public void HideInfo()
+  {
+    Info.SetActive(false);
+    PressStart.SetActive(true);
+    infoShown = false;
+  }
+
+  void Update()
+  {
+    if (infoShown)
+    {
+      return;
+    }
+    if (Input.GetKeyDown(KeyCode.Return))
+    {
+      // load next scene
+      Debug.Log("loading...");
+    }
+  }
+}
